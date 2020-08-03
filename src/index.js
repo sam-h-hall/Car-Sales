2d7361025ace1372ed53f9d69537aeadf8be9d90
sam hall <sam_h_hall@icloud.com>
2020-08-03 13:32:35 -0500

    mvp redo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import "bulma/css/bulma.css";
+import "./styles.scss";
+import { Provider } from "react-redux";
+import { featureReducer } from "./reducer/featureReducer";
+import { createStore } from "redux";
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+const store = createStore(featureReducer);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
